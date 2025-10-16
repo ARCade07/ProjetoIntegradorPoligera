046b096146bc49ae0cbd639a4984029bc30dcf21
danielcruz604 <dancruzsan6@gmail.com>
2025-10-16 18:17:27 -0300

    feat: implementa função de conexão ao banco de dados

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const connectDB = require('./models/db')
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Teste' });
 })
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+connectDB().then(() => {
+    app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+})
 
