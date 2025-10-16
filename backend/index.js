2a2d15c2067c63f692ebe92168ae35613ab20da0
danielcruz604 <dancruzsan6@gmail.com>
2025-10-16 18:10:41 -0300

    chore: importa a dependência dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -6,5 +7,5 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Teste' });
 })
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
 
