971fe5dc2b9d9e0f50eb86ce5437fb1121b42801
danielcruz604 <dancruzsan6@gmail.com>
2025-10-16 17:41:59 -0300

    chore: inicializa servidor express

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Teste' });
+})
+
+app.list(3000, () => console.log('Servidor rodando na porta 3000'))
+
