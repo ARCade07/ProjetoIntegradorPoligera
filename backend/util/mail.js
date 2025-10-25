88487b0976101cb11ca22f3775007cd9db378793
danielcruz604 <dancruzsan6@gmail.com>
2025-10-25 16:10:07 -0300

    feat: adiciona função enviaremail

--- backend/util/mail.js
+++ backend/util/mail.js
@@ -11,3 +11,20 @@ const transporter = nodemailer.createTransport({
         rejectUnauthorized: false
     }
 });
+
+async function enviarEmail(user, resetToken) {
+    resetLink = `http://localhost:8081/reset-password?token=${resetToken}&email=${user.email}`;
+    await transporter.sendMail({
+        from: `"Equipe Poligera" <${process.env.EMAIL_USER}>`,
+        to: user.email,
+        subject: "Recuperação de senha",
+        html: `<p>Olá ${user.name},</p>
+                <p>Você solicitou a redefinição da sua senha.</p>
+                <p>Clique no link abaixo para criar uma nova senha:</p>
+                <a href="${ resetLink }">
+                    Redefinir senha
+                </a>
+                <p>Se você não fez essa solicitação, ignore este e-mail. Sua senha permanecerá inalterada.</p>`
+    });
+}
+
