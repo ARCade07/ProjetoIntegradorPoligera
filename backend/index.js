7944827842bcbbba0022959f6faa19b616dad2c9
danielcruz604 <dancruzsan6@gmail.com>
2025-11-02 23:48:33 -0300

    chore: importa dependência cookie-parser

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 const connectDB = require('./models/db')
 
@@ -9,6 +10,7 @@ const usuarios = require('./routes/usersRoutes');
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Teste' });
