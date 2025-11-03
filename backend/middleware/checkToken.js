194e9c417dd223a677f9c6e922862f1860d886bc
danielcruz604 <dancruzsan6@gmail.com>
2025-11-02 23:53:34 -0300

    refactor: altera verificação do header para cookie

--- backend/middleware/checkToken.js
+++ backend/middleware/checkToken.js
@@ -1,13 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 function checkToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
+    const token = req.cookies?.token;
     if (!token) {
         return res.status(401).json({ msg: 'Acesso negado'});
     }
-
     try {
         const secret = process.env.SECRET;
         jwt.verify(token, secret);
